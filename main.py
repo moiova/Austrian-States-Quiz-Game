38fedf6937bca396e08f8c8c3b8983d96eeb7074
moiova <pingucom1@gmail.com>
2024-04-12 18:43:19 +0200

    creates csv with not guessed states

--- main.py
+++ main.py
@@ -15,15 +15,35 @@ csv_data = pandas.read_csv("9_states.csv")
 csv_data.state = csv_data.state.str.title()
 
 guessed_states = 0
+guessed_states_list = []
 score = Score()
 
+
+def save_not_guessed_states_as_csv():
+    not_guessed_states_list = []
+    state_data_as_list = csv_data.state.to_list()
+
+    # sort lists
+    guessed_states_list.sort()
+    state_data_as_list.sort()
+
+    for i in range(len(state_data_as_list)):
+        if state_data_as_list[i] not in guessed_states_list:
+            not_guessed_states_list.append(state_data_as_list[i])
+
+    df = pandas.DataFrame(not_guessed_states_list)
+    df.to_csv("remaining_states.csv")
+
+
 while guessed_states < 9:
-    state = t.textinput(f"{guessed_states}/{len(csv_data.state)} States Correct", "Give a state")
+    state = t.textinput(f"{guessed_states}/{len(csv_data.state)} States Correct", "Give a state, type 'exit' to exit the game")
 
     if state is None:
         score.game_over()
         guessed_states = 99
 
+    elif state == 'exit':
+        break
     else:
         state = state.title()
         for states_from_csv in csv_data.state:
@@ -32,9 +52,13 @@ while guessed_states < 9:
                 score.update_score(guessed_states)
                 state_data = csv_data[csv_data.state == state]
                 new_state = State(state=state, x_pos=int(state_data.x.iloc[0]), y_pos=int(state_data.y.iloc[0]))
+                guessed_states_list.append(state)
 
 if guessed_states == 9:
     score.congrats()
+
+else:
+    save_not_guessed_states_as_csv()
 
 # screen.update()
 # t.mainloop()
